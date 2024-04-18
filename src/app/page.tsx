81ca7e7d8f7754990a35ff9c5b229c66d4b9d409
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-18 09:42:15 -0300

    Second Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,32 @@
 import Image from "next/image";
 
 
-export default function Home() {
+
+export default async function Home() {
+    const response = await fetch('http://localhost:3000/api/clients');
+    const json = await response.json();
+
+
+
     return (
-        <main>Content</main>
+        <main>
+
+            
+                {json.map((item) => {
+                
+                    return (
+
+                        <>
+                        <div className="a">
+                            <div className="b">{item.name}</div>
+                            <div className="b1">{item.address}</div>
+                        </div>
+                        </>
+                    )
+                })}
+                    
+            
+
+        </main>
     );
 };
